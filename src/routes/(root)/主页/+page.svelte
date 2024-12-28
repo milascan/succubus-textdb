eb7bef9cd288575b7cea86344a377f5003acd784
Lane Sun <lanesun@anlbrain.com>
2024-12-28 18:08:17 +0800

    12-28

--- src/routes/(root)/主页/+page.svelte
+++ src/routes/(root)/主页/+page.svelte
@@ -1,8 +1,11 @@
 <script>
     import Logo from "$lib/images/logo.svg";
+    import Basic from "$lib/layouts/basic.svelte";
 </script>
 
-<div class="box-fill items-center justify-center gap-6">
-    <img alt="logo" src={Logo} class="w-16" />
-    <h1 class="text-3xl">欢迎使用魅魔语料库！</h1>
-</div>
+<Basic>
+    <div class="box-fill items-center justify-center gap-6">
+        <img alt="logo" src={Logo} class="w-16" />
+        <h1 class="text-3xl">欢迎使用魅魔语料库！</h1>
+    </div>
+</Basic>

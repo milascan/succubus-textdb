3643cf809af2ca4f632b0ea745f5903cd5e96d65
Lane Sun <lanesun@anlbrain.com>
2024-12-28 22:34:02 +0800

    12-28

--- src/routes/api/doc/get/+server.js
+++ src/routes/api/doc/get/+server.js
@@ -2,11 +2,15 @@ import { get } from "$lib/db/docs.server.ts";
 import { request_data } from "$lib/utils.ts";
 import { error } from "@sveltejs/kit";
 
-// export const OPTIONS = () => {
-//   return new Response({
-//     headers: { "Access-Control-Allow-Origin": "*" },
-//   });
-// };
+export const OPTIONS = () => {
+  return new Response({
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+};
 
 export const POST = async ({ request }) => {
   try {
